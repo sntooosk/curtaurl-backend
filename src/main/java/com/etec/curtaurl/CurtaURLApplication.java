7ad661b3e10de0fa9a9f80067d835a195fc18865
devtech-alpha <juliano.santos88@etec.sp.gov.br>
2024-07-15 20:40:06 -0300

    add: Cors

--- src/main/java/com/etec/curtaurl/CurtaURLApplication.java
+++ src/main/java/com/etec/curtaurl/CurtaURLApplication.java
@@ -3,10 +3,6 @@ package com.etec.curtaurl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 public class CurtaURLApplication {
