6bae4a99570c272d3fca0283cedcbf79fed5fbed
Juliano Cassimiro <128374665+sntooosk@users.noreply.github.com>
2024-07-16 08:46:29 -0300

    Update URLResponse.java

--- src/main/java/com/etec/curtaurl/models/URLResponse.java
+++ src/main/java/com/etec/curtaurl/models/URLResponse.java
@@ -2,17 +2,10 @@ package com.etec.curtaurl.models;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class URLResponse{
-        String uid;
-        String longUrl;
-        String shortUrl;
-        QrCode qrCode;
-        LocalDateTime createdAt;
-}
+public record URLResponse(
+        String uid,
+        String longUrl,
+        String shortUrl,
+        QrCode qrCode,
+        LocalDateTime createdAt
+) {}
