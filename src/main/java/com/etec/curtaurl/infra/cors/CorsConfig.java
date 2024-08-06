8694daaae97819c362c54697968838211ea657ff
Juliano Cassimiro <128374665+sntooosk@users.noreply.github.com>
2024-08-06 12:38:32 -0300

    Atualizar o CorsConfig.java

--- src/main/java/com/etec/curtaurl/infra/cors/CorsConfig.java
+++ src/main/java/com/etec/curtaurl/infra/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "https://curtaurl-web.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
